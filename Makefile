21321425990e5b882475699046c472995ca6a0f4
idobar1403 <ido1403@gmail.com>
2022-06-19 18:31:13 +0300

    changed

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ CXX=clang++-9
 CXXVERSION=c++2a
 SOURCE_PATH=sources
 OBJECT_PATH=objects
-CXXFLAGS=-std=$(CXXVERSION) -Werror -Wsign-conversion -g -I$(SOURCE_PATH)
+CXXFLAGS=-std=$(CXXVERSION) -Werror -g -Wsign-conversion -I$(SOURCE_PATH)
 TIDY_FLAGS=-extra-arg=-std=$(CXXVERSION) -checks=bugprone-*,clang-analyzer-*,cppcoreguidelines-*,performance-*,portability-*,readability-*,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-cppcoreguidelines-owning-memory --warnings-as-errors=*
 VALGRIND_FLAGS=-v --leak-check=full --show-leak-kinds=all  --error-exitcode=99
 
@@ -14,9 +14,10 @@ SOURCES=$(wildcard $(SOURCE_PATH)/*.cpp)
 HEADERS=$(wildcard $(SOURCE_PATH)/*.hpp)
 OBJECTS=$(subst sources/,objects/,$(subst .cpp,.o,$(SOURCES)))
 
+all:run
 run: test
 
-test: $(OBJECTS)
+test: TestCounter.o Test.o $(OBJECTS)
 	$(CXX) $(CXXFLAGS) $^ -o $@
 
 %.o: %.cpp $(HEADERS)
@@ -24,6 +25,7 @@ test: $(OBJECTS)
 
 $(OBJECT_PATH)/%.o: $(SOURCE_PATH)/%.cpp $(HEADERS)
 	$(CXX) $(CXXFLAGS) --compile $< -o $@
+
 
 tidy:
 	clang-tidy $(SOURCES) $(HEADERS) $(TIDY_FLAGS) --
--- Test.cpp
+++ Test.cpp
@@ -18,8 +18,25 @@ TEST_CASE("make league")
         "Pelicans","Warriors",
     };
     std::vector<double> talent ={0,0.5,0.2,0.1,0.2,0.4,1,0.4,0.7,0.6,0.5,0.3,0.9,0.8,0.6,0.5,0,0.4,0.2,1};
-    // CHECK_NOTHROW(League leag(teamsNames,talent););
+    CHECK_NOTHROW(league::League leag(teamsNames,talent););
     league::League leag(teamsNames,talent);
-    // CHECK_NOTHROW(leag.buildSchedule(););
-    leag.buildSchedule();
+    CHECK_NOTHROW(leag.buildSchedule(););
+    CHECK(leag.names.size()>0);
+    CHECK(leag.talent.size()>0);
+    CHECK(leag.teamsNames.size()>0);
+    CHECK_NOTHROW(leag.leas_points_against());
+    CHECK_NOTHROW(leag.leas_points_for());
+    CHECK_NOTHROW(leag.most_loss());
+    CHECK_NOTHROW(leag.most_negative_points());
+    CHECK_NOTHROW(leag.most_positive_points());
+    CHECK_NOTHROW(leag.most_wins());
+    CHECK(leag.names.size()>0);
+    CHECK(leag.talent.size()>0);
+    CHECK(leag.teamsNames.size()>0);
+    CHECK_NOTHROW(league::Team t1("Lakers"));
+    CHECK_NOTHROW(league::Team t2(""));
+    CHECK_NOTHROW(league::Team t3("Lakers",0.5));
+    CHECK_NOTHROW(league::Team t1("Cavaliers"));
+    CHECK_NOTHROW(league::Team t2(""));
+    CHECK_NOTHROW(league::Team t3("Cavaliers",0.5));
 }
